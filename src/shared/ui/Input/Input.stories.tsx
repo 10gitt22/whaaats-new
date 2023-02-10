0b1bc960d03c9411d169008515744df3ba2860ac
Yevhen Gitt <gitt22konoplyanka@gmail.com>
2023-02-10 22:27:06 +0200

    add input component

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+
+import { Input } from './Input'
+
+const component = {
+  title: 'shared/Input',
+  component: Input,
+  args: {
+    id: 'Username',
+    label: 'Username',
+    type: 'text'
+  }
+}
+
+export default component as ComponentMeta<typeof Input>
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />
+
+export const Primary = Template.bind({})
+export const Dark = Template.bind({})
+Dark.decorators = [ThemeDecorator(Theme.DARK)]
---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,44 @@
+import { FC, InputHTMLAttributes, memo } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import styles from './Input.module.scss'
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'type'>
+
+interface InputProps extends HTMLInputProps {
+  id: string
+  label: string
+  className?: string
+  value?: string
+  type?: 'text' | 'email' | 'number' | 'password' | 'number'
+  onChange?: (value: string) => void
+}
+
+export const Input: FC<InputProps> = memo(({
+  id,
+  label,
+  className,
+  value,
+  onChange,
+  type = 'text',
+  ...props
+}) => {
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value)
+  }
+
+  return (
+    <div className={classNames(styles.Input, {}, [className])}>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        className={styles.inputField}
+        onChange={onChangeHandler}
+        placeholder=" "
+        autoComplete="off"
+        {...props}
+      />
+      <label className={styles.inputLabel}>{label}</label>
+    </div>
+  )
+})
